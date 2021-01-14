b960ac0609e95810487426c37712ed01da02ccbc
kimxwan0319 <kimxwan0319@naver.com>
2021-01-14 23:30:01 +0900

    Initial Commit

---
+++ DDYZD_V2/DDYZD_V2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DDYZD_V2
+//
+//  Created by 김수완 on 2021/01/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
