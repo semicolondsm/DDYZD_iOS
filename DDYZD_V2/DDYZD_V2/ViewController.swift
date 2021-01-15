d5c81751f5a262ef3bdfb49072301578a48a0a2f
kimxwan0319 <kimxwan0319@naver.com>
2021-01-15 11:09:17 +0900

    DELETE; SceneDelegate

--- DDYZD_V2/DDYZD_V2/ViewController.swift
+++ DDYZD_V2/DDYZD_V2/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var window: UIWindow?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
