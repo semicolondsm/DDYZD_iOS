2053f51354cedb970a93ddaef0d0030a73701a7d
kimxwan0319 <kimxwan0319@naver.com>
2021-01-17 19:49:21 +0900

    UPDATE; DDYZDAPI > AuthAPIs

--- DDYZD_V2/DDYZD_V2/Source/Networking/DDYZDAPI.swift
+++ DDYZD_V2/DDYZD_V2/Source/Networking/DDYZDAPI.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+enum DDYZDAPI {
+    
+    //Auth
+    case getToken
+    case postDeviceToken
+    case refreshToken
+    case userInfo(_ gcn: String)
+    case updateInfo
+    
+    func path() -> String {
+        switch self {
+        case .getToken:
+            return "/users/token"
+        case .postDeviceToken:
+            return "/users/device_token"
+        case .refreshToken:
+            return "/users/refresh"
+        case .userInfo(let gcn):
+            return "/users/"+gcn
+        case .updateInfo:
+            return "/users/profile"
+        }
+    }
+}
