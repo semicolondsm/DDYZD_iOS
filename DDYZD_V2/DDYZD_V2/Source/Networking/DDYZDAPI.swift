abc079e45d943668099e295c51246354ce3888d9
kimxwan0319 <kimxwan0319@naver.com>
2021-01-18 00:11:51 +0900

    UPDATE; DDYZDAPI's descriptions

--- DDYZD_V2/DDYZD_V2/Source/Networking/DDYZDAPI.swift
+++ DDYZD_V2/DDYZD_V2/Source/Networking/DDYZDAPI.swift
@@ -11,20 +11,23 @@ import Alamofire
 enum DDYZDAPI {
     
     //Auth
-    case getToken(_ DSMAuthToken: String)
-    case postDeviceToken
-    case refreshToken
-    case userInfo(_ gcn: String)
-    case updateInfo
+    case getToken(_ DSMAuthToken: String)   // 토큰 발급
+    case postDeviceToken                    // 디바이스 토큰 입력
+    case refreshToken                       // 토큰 재발급
+    case userInfo(_ gcn: String)            // 유저 정보
+    case updateInfo                         // 프로필 수정
     
     //club
-    case clubList
-    case clubDetailInfo(_ clubID: Int)
-    case getRecruitment(_ clubID: Int)
-    case getClubMember(_ clubID: Int)
-    case cancelFollow(_ clubID: Int)
-    case exitClub(_ clubID: Int)
-    case FollowClub(_ clubID: Int)
+    case clubList                           // 동아리 리스트 반환
+    case clubDetailInfo(_ clubID: Int)      // 동아리 상세 정보
+    case getRecruitment(_ clubID: Int)      // 모집 공고 정보 반환
+    case getClubMember(_ clubID: Int)       // 동아리 멤버
+    case cancelFollow(_ clubID: Int)        // 동아리 팔로우 취소
+    case exitClub(_ clubID: Int)            // 소속 동아리 나가기
+    case followClub(_ clubID: Int)          // 동아리 팔로우
+    
+    //head authority
+    case makeRecruitment(_ clubID: Int)     // 동아리 모집 공고 생성
     
     
     func path() -> String {
@@ -51,8 +54,10 @@ enum DDYZDAPI {
             return "/club/\(clubID)/follow"
         case .exitClub(let clubID):
             return "/club/\(clubID)"
-        case .FollowClub(let clubID):
+        case .followClub(let clubID):
             return "/club/\(clubID)/follow"
+        case .makeRecruitment(let clubID):
+            return "/club/\(clubID)/recruitment"
         }
     }
     
