1a67bfed6f55e8a41f8b51cc5715cadc793795c4
kimxwan0319 <kimxwan0319@naver.com>
2021-01-17 15:48:44 +0900

    UPDATE; install 'RxSwift', 'RxCocoa'

--- DDYZD_V2/Pods/Target Support Files/Pods-DDYZD_V2/Pods-DDYZD_V2-frameworks.sh
+++ DDYZD_V2/Pods/Target Support Files/Pods-DDYZD_V2/Pods-DDYZD_V2-frameworks.sh
@@ -199,10 +199,16 @@ fi
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DSMSDK/DSMSDK.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DSMSDK/DSMSDK.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
