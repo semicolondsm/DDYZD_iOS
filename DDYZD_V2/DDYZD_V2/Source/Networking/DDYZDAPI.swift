32877fe3164d855e96ac8868d64e574aad64fa82
kimxwan0319 <kimxwan0319@naver.com>
2021-01-17 23:54:27 +0900

    UPDATE; DDYZDAPI about auth, club

--- DDYZD_V2/DDYZD_V2/Source/Networking/DDYZDAPI.swift
+++ DDYZD_V2/DDYZD_V2/Source/Networking/DDYZDAPI.swift
@@ -6,28 +6,67 @@
 //
 
 import Foundation
+import Alamofire
 
 enum DDYZDAPI {
     
     //Auth
-    case getToken
+    case getToken(_ DSMAuthToken: String)
     case postDeviceToken
     case refreshToken
     case userInfo(_ gcn: String)
     case updateInfo
     
+    //club
+    case clubList
+    case clubDetailInfo(_ clubID: Int)
+    case getRecruitment(_ clubID: Int)
+    case getClubMember(_ clubID: Int)
+    case cancelFollow(_ clubID: Int)
+    case exitClub(_ clubID: Int)
+    case FollowClub(_ clubID: Int)
+    
+    
     func path() -> String {
         switch self {
-        case .getToken:
+        case .getToken(_):
             return "/users/token"
         case .postDeviceToken:
             return "/users/device_token"
         case .refreshToken:
             return "/users/refresh"
         case .userInfo(let gcn):
-            return "/users/"+gcn
+            return "/users/\(gcn)"
         case .updateInfo:
             return "/users/profile"
+        case .clubList:
+            return "/club/list"
+        case .clubDetailInfo(let clubID):
+            return "/club/\(clubID)"
+        case .getRecruitment(let clubID):
+            return "/club/\(clubID)/recruitment"
+        case .getClubMember(let clubID):
+            return "/club/\(clubID)/member"
+        case .cancelFollow(let clubID):
+            return "/club/\(clubID)/follow"
+        case .exitClub(let clubID):
+            return "/club/\(clubID)"
+        case .FollowClub(let clubID):
+            return "/club/\(clubID)/follow"
+        }
+    }
+    
+    func header() -> HTTPHeaders? {
+        switch self {
+        case .getToken(let DSMAuthToken) :
+            return ["access_token": "Bearer \(DSMAuthToken)"]
+        case .refreshToken :
+            guard let refreshToken = UserDefaults.standard.string(forKey: "refreshToken") else {
+                return nil
+            }
+            return ["refresh-token": "Bearer \(refreshToken)"]
+        default:
+            return nil
         }
     }
 }
