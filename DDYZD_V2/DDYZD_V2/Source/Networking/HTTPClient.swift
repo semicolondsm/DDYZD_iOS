cb5e143eb99a32557f5b2f41c0fc2e3500a9b017
kimxwan0319 <kimxwan0319@naver.com>
2021-01-17 21:51:14 +0900

    UPDATE; REST API  method functions

--- DDYZD_V2/DDYZD_V2/Source/Networking/HTTPClient.swift
+++ DDYZD_V2/DDYZD_V2/Source/Networking/HTTPClient.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import RxSwift
+import Alamofire
+import RxAlamofire
+
+class HTTPClient {
+    
+    let baseURL = ""
+    
+    typealias httpResult = Observable<(HTTPURLResponse, Data)>
+    
+    func get(_ api: DDYZDAPI, param: Parameters?) -> httpResult {
+        return requestData(.get, baseURL + api.path(),
+                           parameters: param,
+                           encoding: JSONEncoding.prettyPrinted,
+                           headers: api.header())
+    }
+    
+    func post(_ api: DDYZDAPI, param: Parameters?, encoding: ParameterEncoding = JSONEncoding.default) -> httpResult {
+        return requestData(.post, baseURL + api.path(),
+                           parameters: param,
+                           encoding: encoding,
+                           headers: api.header())
+    }
+    
+    func put(_ api: DDYZDAPI, param: Parameters?) -> httpResult {
+        return requestData(.put, baseURL + api.path(),
+                           parameters: param,
+                           encoding: JSONEncoding.prettyPrinted,
+                           headers: api.header())
+    }
+    
+    func delete(_ api: DDYZDAPI, param: Parameters?) -> httpResult {
+        return requestData(.delete, baseURL + api.path(),
+                           parameters: param,
+                           encoding: JSONEncoding.prettyPrinted,
+                           headers: api.header())
+    }
+}
+
