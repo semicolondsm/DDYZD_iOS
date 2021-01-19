8a3fe088173af9026ef6502d2a71a27972d92ffb
kimxwan0319 <kimxwan0319@naver.com>
2021-01-19 21:50:38 +0900

    UPDATE; Login VC UI

--- DDYZD_V2/DDYZD_V2/Source/View/LoginViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/LoginViewController.swift
@@ -7,12 +7,31 @@
 
 import UIKit
 
+import AuthenticationServices
+import DSMSDK
+import RxCocoa
+import RxSwift
+
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var appleAuthProvider: UIStackView!
+    @IBOutlet weak var DSMAuthProvider: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupProviserLoginViews()
+    }
+    
+    func setupProviserLoginViews(){
+        let DSMAuthBtn = UIButton()
+        DSMAuthBtn.setBackgroundImage(UIImage(named: "DSMAuthLoginBtn"), for: .normal)
+        
+        let appleAuthBtn = ASAuthorizationAppleIDButton(authorizationButtonType: .signIn, authorizationButtonStyle: .whiteOutline)
+        appleAuthBtn.cornerRadius = 50
+        
+        self.DSMAuthProvider.addArrangedSubview(DSMAuthBtn)
+        self.appleAuthProvider.addArrangedSubview(appleAuthBtn)
     }
 
 }
