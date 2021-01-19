1d00c019541596c0b3b1477c672391f0d4f405e2
kimxwan0319 <kimxwan0319@naver.com>
2021-01-19 23:19:00 +0900

    CREATE; LoginViewModel

--- DDYZD_V2/DDYZD_V2/Source/View/LoginViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/LoginViewController.swift
@@ -17,6 +17,8 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var appleAuthProvider: UIStackView!
     @IBOutlet weak var DSMAuthProvider: UIStackView!
     
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,6 +28,18 @@ class LoginViewController: UIViewController {
     func setupProviserLoginViews(){
         let DSMAuthBtn = UIButton()
         DSMAuthBtn.setBackgroundImage(UIImage(named: "DSMAuthLoginBtn"), for: .normal)
+        
+        DSMAuthBtn.rx.tap.subscribe(onNext: {
+            DSMAUTH.loginWithDSMAuth(vc: self){ (token,error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print(token!)
+                }
+            }
+        })
+        .disposed(by: disposeBag)
         
         let appleAuthBtn = ASAuthorizationAppleIDButton(authorizationButtonType: .signIn, authorizationButtonStyle: .whiteOutline)
         appleAuthBtn.cornerRadius = 50
---
+++ DDYZD_V2/DDYZD_V2/Source/ViewModel/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  DDYZD_V2
+//
+//  Created by 김수완 on 2021/01/19.
+//
+
+import Foundation
