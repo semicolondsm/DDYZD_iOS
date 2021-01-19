0fbd3273582eca16d6a1111df64431d20b295b78
kimxwan0319 <kimxwan0319@naver.com>
2021-01-19 12:10:25 +0900

    CREATE; LoginViewController

---
+++ DDYZD_V2/DDYZD_V2/Source/View/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  DDYZD_V2
+//
+//  Created by 김수완 on 2021/01/19.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
