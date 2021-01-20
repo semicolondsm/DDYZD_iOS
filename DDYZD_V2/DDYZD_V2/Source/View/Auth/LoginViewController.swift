8af4030376e6800d1351b59c8519261a06d8b786
kimxwan0319 <kimxwan0319@naver.com>
2021-01-20 23:05:38 +0900

    UPDATE; LoginViewController DSMAuth

--- DDYZD_V2/DDYZD_V2/Source/View/Auth/LoginViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Auth/LoginViewController.swift
@@ -17,35 +17,31 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var appleAuthProvider: UIStackView!
     @IBOutlet weak var DSMAuthProvider: UIStackView!
     
-    var disposeBag = DisposeBag()
+    let viewModel = LoginViewModel()
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupProviserLoginViews()
+        bind()
     }
     
-    func setupProviserLoginViews(){
+    func bind(){
         let DSMAuthBtn = UIButton()
         DSMAuthBtn.setBackgroundImage(UIImage(named: "DSMAuthLoginBtn"), for: .normal)
-        
-        DSMAuthBtn.rx.tap.subscribe(onNext: {
-            DSMAUTH.loginWithDSMAuth(vc: self){ (token,error) in
-                if let error = error {
-                    print(error)
-                }
-                else {
-                    print(token!)
-                }
-            }
-        })
-        .disposed(by: disposeBag)
         
         let appleAuthBtn = ASAuthorizationAppleIDButton(authorizationButtonType: .signIn, authorizationButtonStyle: .whiteOutline)
         appleAuthBtn.cornerRadius = 50
         
         self.DSMAuthProvider.addArrangedSubview(DSMAuthBtn)
         self.appleAuthProvider.addArrangedSubview(appleAuthBtn)
+        
+        let input = LoginViewModel.input.init(vc: self, loginWithDSMAuthBtnDriver: DSMAuthBtn.rx.tap.asDriver())
+        let output = viewModel.transform(input)
+        
+        output.result
     }
+    
+    
 
 }
