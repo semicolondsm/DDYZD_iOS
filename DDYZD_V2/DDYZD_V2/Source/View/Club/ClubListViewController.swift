4b9c28435c35650808645e18a5b716cc8856902a
kimxwan0319 <kimxwan0319@naver.com>
2021-01-21 20:28:04 +0900

    CREATE; ClubListViewController > setTopTabbar()

--- DDYZD_V2/DDYZD_V2/Source/View/Club/ClubListViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Club/ClubListViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-
 import Parchment
 
 class ClubListViewController: UIViewController {
@@ -19,7 +18,23 @@ class ClubListViewController: UIViewController {
     
 
     func setTopTabbar(){
+        let storyboard = UIStoryboard(name: "Club", bundle: nil)
+        let firstVC = storyboard.instantiateViewController(withIdentifier: "CluListTableViewController") as! CluListTableViewController
+        firstVC.title = "웹"
+        let secondVC = storyboard.instantiateViewController(withIdentifier: "CluListTableViewController") as! CluListTableViewController
+        secondVC.title = "앱"
         
+        let pagingViewController = PagingViewController(viewControllers: [
+            firstVC,
+            secondVC
+        ])
+        
+        // Make sure you add the PagingViewController as a child view
+        // controller and contrain it to the edges of the view.
+        addChild(pagingViewController)
+        view.addSubview(pagingViewController.view)
+        view.constrainToEdges(pagingViewController.view)
+        pagingViewController.didMove(toParent: self)
     }
 
 }
