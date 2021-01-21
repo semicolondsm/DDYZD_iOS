1b04a7b713d14ccfb227e2a2294ff1f159b94530
kimxwan0319 <kimxwan0319@naver.com>
2021-01-21 20:16:20 +0900

    UPDATE; set storyboard

--- DDYZD_V2/DDYZD_V2/Source/View/Club/ClubListViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Club/ClubListViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import Parchment
+
 class ClubListViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -16,14 +18,8 @@ class ClubListViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setTopTabbar(){
+        
     }
-    */
 
 }
