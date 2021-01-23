d6a440143bff69b7f0aa422b9169267a2030f29a
kimxwan0319 <kimxwan0319@naver.com>
2021-01-24 02:04:47 +0900

    UPDATE; ClubListTableViewController

--- DDYZD_V2/DDYZD_V2/Source/View/Club/ClubListTableViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Club/ClubListTableViewController.swift
@@ -7,12 +7,41 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+import Kingfisher
+
 class ClubListTableViewController: UIViewController {
 
+    @IBOutlet weak var ClubListTable: UITableView!
+    
     public var clubTag: ClubListCategory = .all
+    private let viewModel = ClubListViewModel()
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUI()
+        bind()
+    }
+    
+    func setUI(){
+        ClubListTable.separatorStyle = .none
+    }
+    
+    func bind(){
+        let input = ClubListViewModel.input(
+            clubTag: clubTag,
+            selectIndexPath: ClubListTable.rx.itemSelected.asSignal()
+        )
+        let output = viewModel.transform(input)
+        
+        output.clubList.bind(to: ClubListTable.rx.items(cellIdentifier: "ClubListTableViewCell", cellType: ClubListTableViewCell.self)){ row, item, cell in
+            cell.clubNameLable.text = item.clubname
+            cell.clubDescription.text = item.clubdescription
+        }
+        .disposed(by: disposeBag)
     }
 
 }
