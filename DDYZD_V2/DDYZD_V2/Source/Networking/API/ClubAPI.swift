b938fc229f9487e71f4b175877dbfe9f5b5d6158
kimxwan0319 <kimxwan0319@naver.com>
2021-01-24 00:28:16 +0900

    UPDATE; getClubList function

--- DDYZD_V2/DDYZD_V2/Source/Networking/API/ClubAPI.swift
+++ DDYZD_V2/DDYZD_V2/Source/Networking/API/ClubAPI.swift
@@ -10,8 +10,20 @@ import Foundation
 import Alamofire
 import RxSwift
 
+
 class ClubAPI {
     let httpClient = HTTPClient()
     
-    
+    func getClubList() -> Observable<([ClubListModel]? ,StatusCodes)> {
+        httpClient.get(.clubList, param: nil)
+            .map{ response, data -> ([ClubListModel]?, StatusCodes) in
+                switch response.statusCode {
+                case 200:
+                    guard let data = try? JSONDecoder().decode([ClubListModel].self, from: data) else { return (nil, .fault)}
+                    return(data, .success)
+                default:
+                    return (nil, .fault)
+                }
+            }
+    }
 }
