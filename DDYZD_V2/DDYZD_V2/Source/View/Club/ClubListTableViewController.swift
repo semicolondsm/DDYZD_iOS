b49e711c657df0187c3dd51f48254d1e8465caee
kimxwan0319 <kimxwan0319@naver.com>
2021-01-23 16:56:16 +0900

    UPDATE; clubTag method in ClubListTableViewController

--- DDYZD_V2/DDYZD_V2/Source/View/Club/ClubListTableViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Club/ClubListTableViewController.swift
@@ -7,21 +7,12 @@
 
 import UIKit
 
-class CluListTableViewController: UITableViewController {
+class ClubListTableViewController: UIViewController {
 
+    public var clubTag: ClubListCategory = .all
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
     }
 
 }
