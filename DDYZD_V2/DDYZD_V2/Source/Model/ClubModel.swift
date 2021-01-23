8522d1325dbeae5e5784d0942a5e0da6513d47b3
kimxwan0319 <kimxwan0319@naver.com>
2021-01-23 16:52:55 +0900

    UPDATE; ClubListCategory enum

--- DDYZD_V2/DDYZD_V2/Source/Model/ClubModel.swift
+++ DDYZD_V2/DDYZD_V2/Source/Model/ClubModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+enum ClubListCategory {
+    case all
+    case web
+    case app
+    case embedded
+    case ect
+    case CA
+    
+    func title() -> String{
+        switch  self {
+        case .all:
+            return "전체"
+        case .web:
+            return "웹"
+        case .app:
+            return "앱"
+        case .embedded:
+            return "임베디드"
+        case .ect:
+            return "기타"
+        case .CA:
+            return "창체"
+        }
+    }
+    
+    static let allItems = [all,web,app,embedded,ect,CA]
+}
