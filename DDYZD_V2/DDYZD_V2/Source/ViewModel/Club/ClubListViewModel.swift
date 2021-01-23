d50993a57747d07e6cc48be6b8e8d74129620773
kimxwan0319 <kimxwan0319@naver.com>
2021-01-24 00:25:43 +0900

    UPDATE; ClubListViewModel

--- DDYZD_V2/DDYZD_V2/Source/ViewModel/Club/ClubListViewModel.swift
+++ DDYZD_V2/DDYZD_V2/Source/ViewModel/Club/ClubListViewModel.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+import RxCocoa
+import RxSwift
+
+class ClubListViewModel: ViewModelProtocol {
+    
+    let disposeBag = DisposeBag()
+    
+    struct input {
+        let clubTag: ClubListCategory
+    }
+    
+    struct output {
+        let result: Observable<String>
+        let clubList: PublishRelay<[ClubListModel]>
+    }
+    
+    func transform(_ input: input) -> output {
+        let api = ClubAPI()
+        let result = PublishSubject<String>()
+        let clubList = PublishRelay<[ClubListModel]>()
+        
+        api.getClubList().subscribe(onNext:{ data, response in
+            switch response {
+            case .success:
+                var returnClubList = [ClubListModel]()
+                
+                for club in data! {
+                    for tag in club.clubTag {
+                        if input.clubTag.classificationTag(tag) {
+                            returnClubList.append(club)
+                            break
+                        }
+                    }
+                }
+
+                clubList.accept(returnClubList)
+                result.onCompleted()
+                
+            default:
+                result.onNext("API 'getClubList' ERROR")
+            }
+        })
+        .disposed(by: disposeBag)
+        
+        return output(result: result, clubList: clubList)
+    }
+    
+}
