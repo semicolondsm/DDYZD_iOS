81598936725fca42bd8f0162f89035a2e5f6cea1
kimxwan0319 <kimxwan0319@naver.com>
2021-01-23 13:52:20 +0900

    CREATE; files about ClubListTableViewCell

---
+++ DDYZD_V2/DDYZD_V2/Source/View/Club/ClubListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ClubListTableViewCell.swift
+//  DDYZD_V2
+//
+//  Created by 김수완 on 2021/01/23.
+//
+
+import UIKit
+
+class ClubListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
