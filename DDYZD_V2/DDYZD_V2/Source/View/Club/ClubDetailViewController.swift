a5eee97d664ccb67a40e245b2ca4f66d2cc71bf3
kimxwan0319 <kimxwan0319@naver.com>
2021-01-24 16:04:25 +0900

    CREATE; files about ClubDetail page

---
+++ DDYZD_V2/DDYZD_V2/Source/View/Club/ClubDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ClubDetailViewController.swift
+//  DDYZD_V2
+//
+//  Created by 김수완 on 2021/01/24.
+//
+
+import UIKit
+
+class ClubDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
