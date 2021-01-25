84077f6997b76e8a86a43e2bc4ae9bd476542267
kimxwan0319 <kimxwan0319@naver.com>
2021-01-25 22:49:43 +0900

    CREATE; file and view controller in storyboard about club list

---
+++ DDYZD_V2/DDYZD_V2/Source/View/Chat/ChatListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatListViewController.swift
+//  DDYZD_V2
+//
+//  Created by 김수완 on 2021/01/25.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
