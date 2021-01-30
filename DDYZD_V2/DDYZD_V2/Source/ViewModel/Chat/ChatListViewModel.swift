ff3540213819f73a9c1a5e760b178cbb92e0059e
kimxwan0319 <kimxwan0319@naver.com>
2021-01-30 18:50:52 +0900

    UPDATE; viewModel about get chat list

--- DDYZD_V2/DDYZD_V2/Source/ViewModel/Chat/ChatListViewModel.swift
+++ DDYZD_V2/DDYZD_V2/Source/ViewModel/Chat/ChatListViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+import RxCocoa
+import RxSwift
+
+class ChatListViewModel: ViewModelProtocol {
+    
+    let disposeBag = DisposeBag()
+    
+    struct input {
+    }
+    
+    struct output {
+        let result: Observable<String>
+        let chatList: PublishRelay<[ChatRoom]>
+    }
+    
+    func transform(_ input: input) -> output {
+        let api = ChatAPI()
+        let result = PublishSubject<String>()
+        let chatList = PublishRelay<[ChatRoom]>()
+        
+        api.getChatList().subscribe(onNext: { data, response in
+            switch response {
+            case .success:
+                chatList.accept(data!)
+                result.onCompleted()
+            default:
+                result.onNext("API 'getChatList' ERROR")
+            }
+        })
+        .disposed(by: disposeBag)
+        
+        return output(result: result, chatList: chatList)
+    }
+}
