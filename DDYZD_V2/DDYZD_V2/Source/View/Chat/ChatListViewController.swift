24c52167c3aea247471a73ad79d3293359536c02
kimxwan0319 <kimxwan0319@naver.com>
2021-01-30 22:18:30 +0900

    UPDATE; connect viewModel

--- DDYZD_V2/DDYZD_V2/Source/View/Chat/ChatListViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Chat/ChatListViewController.swift
@@ -16,6 +16,9 @@ class ChatListViewController: UIViewController {
 
     @IBOutlet weak var ChatListTable: UITableView!
     
+    private let viewModel = ChatListViewModel()
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
