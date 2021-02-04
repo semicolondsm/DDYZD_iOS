8dc961111e79ed28d3e27a7cb6114f6a98d396b9
kimxwan0319 <kimxwan0319@naver.com>
2021-02-04 19:46:21 +0900

    CREATE; ClubDetailViewController

---
+++ DDYZD_V2/DDYZD_V2/Source/View/Club/ClubDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ClubDetailViewController.swift
+//  DDYZD_V2
+//
+//  Created by 김수완 on 2021/02/04.
+//
+
+import UIKit
+
+class ClubDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
