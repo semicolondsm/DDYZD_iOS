08360deea3395b3a1b50b1957b67366711d3849d
kimxwan0319 <kimxwan0319@naver.com>
2021-02-04 19:48:36 +0900

    CREATE; ClubDetailViewModel

---
+++ DDYZD_V2/DDYZD_V2/Source/ViewModel/Club/ClubDetailViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ClubDetailViewModel.swift
+//  DDYZD_V2
+//
+//  Created by 김수완 on 2021/02/04.
+//
+
+import Foundation
+
+class ClubDetailViewModel: ViewModelProtocol {
+    
+    struct input {
+    }
+    
+    struct output {
+    }
+    
+    func transform(_ input: input) -> output {
+        return output()
+    }
+}
