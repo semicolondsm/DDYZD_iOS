eeca681061617df45c282c354e25944913cf64db
kimxwan0319 <kimxwan0319@naver.com>
2021-02-07 00:20:00 +0900

    DELETE; about main feed web view

--- DDYZD_V2/DDYZD_V2/Source/View/Main/MainPageViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Main/MainPageViewController.swift
@@ -24,7 +24,6 @@ class MainPageViewController: UIViewController {
         
         bind()
         refreshToken()
-        setWebView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
