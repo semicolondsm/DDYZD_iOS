5211299c3e1c63945689f6070649a354a70fb654
kimxwan0319 <kimxwan0319@naver.com>
2021-02-07 00:57:44 +0900

    CREATE; Models ( FeedListModel, Medium )

--- DDYZD_V2/DDYZD_V2/Source/Model/FeedModel.swift
+++ DDYZD_V2/DDYZD_V2/Source/Model/FeedModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct FeedListModel: Codable {
+    let feedId: Int
+    let clubName: String
+    let profileImage: String
+    let isFollow: Bool
+    let uploadAt: String
+    let content: String
+    let media: [medium]
+    let isFlag: Bool
+    let flags: Int
+}
+
+struct Medium: Codable {
+    let medium: String
+}
