13be01a70114af39a3eff6d0fd8260138544f827
kimxwan0319 <kimxwan0319@naver.com>
2021-02-07 01:42:30 +0900

    UPDATE; create and bind header reusableView

---
+++ DDYZD_V2/DDYZD_V2/Source/View/Main/HeaderReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  HeaderReusableView.swift
+//  DDYZD_V2
+//
+//  Created by 김수완 on 2021/02/07.
+//
+
+import UIKit
+
+class HeaderReusableView: UICollectionReusableView {
+    @IBOutlet weak var headerWKView: WKWebView!
+}
