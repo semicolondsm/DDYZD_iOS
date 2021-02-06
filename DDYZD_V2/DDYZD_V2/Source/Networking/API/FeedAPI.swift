d3eead81b4a8750162ca7e9501845ca48fe59115
kimxwan0319 <kimxwan0319@naver.com>
2021-02-07 01:14:35 +0900

    UPDATE; getFeedList function

--- DDYZD_V2/DDYZD_V2/Source/Networking/API/FeedAPI.swift
+++ DDYZD_V2/DDYZD_V2/Source/Networking/API/FeedAPI.swift
@@ -13,4 +13,16 @@ import RxSwift
 class FeedAPI {
     let httpClient = HTTPClient()
     
+    func getFeedList(page: Int) -> Observable<([FeedListModel]?, StatusCodes)> {
+        httpClient.get(.feedList(page), param: nil)
+            .map{ response, data -> ([FeedListModel]?, StatusCodes) in
+                switch response.statusCode {
+                case 200:
+                    guard let data = try? JSONDecoder().decode([FeedListModel].self, from: data) else { return (nil, .fault) }
+                    return (data, .success)
+                default:
+                    return (nil, .fault)
+                }
+            }
+    }
 }
