292b283471405200b37fddae028ae784a20e5160
kimxwan0319 <kimxwan0319@naver.com>
2021-02-07 23:16:17 +0900

    UPDATE; bind UI

--- DDYZD_V2/DDYZD_V2/Source/View/Main/FeedCell/FeedTableViewCell.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Main/FeedCell/FeedTableViewCell.swift
@@ -9,15 +9,22 @@ import UIKit
 
 class FeedTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var clubProfileImageView: UIImageView!
+    @IBOutlet weak var clubNameLable: UILabel!
+    @IBOutlet weak var uploadAtLable: UILabel!
+    @IBOutlet weak var conectLable: UILabel!
+    @IBOutlet weak var flagNumLable: UILabel!
+    @IBOutlet weak var MenuBtn: UIButton!
+    @IBOutlet weak var flagBtn: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        setUI()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func setUI() {
+        clubProfileImageView.layer.cornerRadius = 30
     }
 
 }
