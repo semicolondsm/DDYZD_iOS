bda3094bc330f1676ddabed1a5194e4a13ac415b
kimxwan0319 <kimxwan0319@naver.com>
2021-02-14 02:56:05 +0900

    UPDATE; set view controller

--- DDYZD_V2/DDYZD_V2/Source/View/Club/ClubDetailViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Club/ClubDetailViewController.swift
@@ -7,11 +7,26 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+
 class ClubDetailViewController: UIViewController {
 
+    public var clubID = 0
+    
+    @IBOutlet weak var feedTable: UITableView!
+    
+    private let viewModel = ClubDetailViewModel()
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        bind()
+    }
+    
+    func bind() {
+        
     }
     
 
