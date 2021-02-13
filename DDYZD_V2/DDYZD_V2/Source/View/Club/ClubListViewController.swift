f1de80f7baebe320c82a8855eb88d8ceaa492564
kimxwan0319 <kimxwan0319@naver.com>
2021-02-14 04:11:02 +0900

    REFACT; setNavigationBar()

--- DDYZD_V2/DDYZD_V2/Source/View/Club/ClubListViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Club/ClubListViewController.swift
@@ -25,17 +25,15 @@ class ClubListViewController: TabmanViewController {
     func setNavigationBar(){
         navigationController?.navigationBar.shadowImage = UIImage()
         
-        let label = UILabel()
-            
-        label.translatesAutoresizingMaskIntoConstraints = false
+        let customView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 100.0, height: 44.0))
+        let label = UILabel(frame: CGRect(x: -15.0, y: 0.0, width: 150.0, height: 44.0))
         label.text = "동아리 리스트"
         label.font = UIFont.systemFont(ofSize: 25)
-        label.textAlignment = .left
-        navigationItem.titleView = label
-            
-        if let navigationBar = navigationController?.navigationBar{
-            label.widthAnchor.constraint(equalTo: navigationBar.widthAnchor, constant: -40).isActive = true
-        }
+        label.textAlignment = NSTextAlignment.right
+        customView.addSubview(label)
+        
+        let leftButton = UIBarButtonItem(customView: customView)
+        self.navigationItem.leftBarButtonItem = leftButton
     }
     
     func embeddViewControllers(){
