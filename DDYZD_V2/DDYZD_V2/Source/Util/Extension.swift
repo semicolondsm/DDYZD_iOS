a2e99d6e7d64a6e9b2007bfde4d1547dfa66f9bf
kimxwan0319 <kimxwan0319@naver.com>
2021-02-14 23:11:23 +0900

    UPDATE; append extension about roundCorners at UIImageView

--- DDYZD_V2/DDYZD_V2/Source/Util/Extension.swift
+++ DDYZD_V2/DDYZD_V2/Source/Util/Extension.swift
@@ -103,6 +103,13 @@ extension UIImageView {
     func circleImage() {
         self.layer.cornerRadius = (self.frame.height + self.frame.width)/4
     }
+    
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
 }
 
 extension UILabel {
