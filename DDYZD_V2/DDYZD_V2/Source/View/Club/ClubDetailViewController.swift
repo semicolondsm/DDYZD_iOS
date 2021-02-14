354bd4a6a490763c77be5d5ebb467a9ca1d8fcfa
kimxwan0319 <kimxwan0319@naver.com>
2021-02-14 23:19:33 +0900

    UPDATE; set navigationbar, UI

--- DDYZD_V2/DDYZD_V2/Source/View/Club/ClubDetailViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Club/ClubDetailViewController.swift
@@ -15,25 +15,38 @@ class ClubDetailViewController: UIViewController {
     public var clubID = 0
     
     @IBOutlet weak var feedTable: UITableView!
+    @IBOutlet weak var clubBackImage: UIImageView!
+    @IBOutlet weak var clubProfileImgae: UIImageView!
+    @IBOutlet weak var clubNameLabel: UILabel!
     
     private let viewModel = ClubDetailViewModel()
     private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setNavigationBar()
+        
+        setUI()
         bind()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setNavigationBar()
     }
     
     func bind() {
         
     }
     
+    func setUI(){
+        clubProfileImgae.circleImage()
+    }
+    
     func setNavigationBar(){
         navigationController?.navigationBar.standardAppearance.shadowColor = .gray
         navigationController?.navigationBar.standardAppearance.backgroundColor = .white
-        self.title = String(clubID)
+        navigationController?.navigationBar.topItem?.title = ""
+        navigationController?.navigationBar.tintColor = #colorLiteral(red: 0.4811326265, green: 0.1003668979, blue: 0.812384963, alpha: 1)
     }
 
 }
+
