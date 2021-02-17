247bde193ea2e52d534e2d289cbc2caa998d5089
kimxwan0319 <kimxwan0319@naver.com>
2021-02-17 23:02:35 +0900

    UPDATE; bind views

--- DDYZD_V2/DDYZD_V2/Source/View/Personal/MyPageViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Personal/MyPageViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class MyPageViewController: UIViewController {
 
+    @IBOutlet weak var menuBtn: UIBarButtonItem!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var gcnLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var githubLinkBtn: UIButton!
+    @IBOutlet weak var belongClubCollectionView: UICollectionView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
