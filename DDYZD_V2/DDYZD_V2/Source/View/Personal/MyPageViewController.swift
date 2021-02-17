d512a173c905844dc0eac330a38ba13438fdb6cb
kimxwan0319 <kimxwan0319@naver.com>
2021-02-17 18:34:27 +0900

    CREATE; ViewController and ViewModel about MyPage

---
+++ DDYZD_V2/DDYZD_V2/Source/View/Personal/MyPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyPageViewController.swift
+//  DDYZD_V2
+//
+//  Created by 김수완 on 2021/02/17.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
