87383117a328fc8b73d63f48344619076d1ace23
kimxwan0319 <kimxwan0319@naver.com>
2021-02-19 01:40:07 +0900

    UPDATE; keyboard event

--- DDYZD_V2/DDYZD_V2/Source/View/Chat/ChatViewController.swift
+++ DDYZD_V2/DDYZD_V2/Source/View/Chat/ChatViewController.swift
@@ -14,20 +14,52 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var messageTextField: UITextField!
     @IBOutlet weak var sendBtn: UIButton!
     
+    private var keyboardHeight: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        addKeyboardNotification()
     }
 
 }
 
 //MARK:- UI
-
 extension ChatViewController {
     func setUI(){
         textInputView.layer.borderWidth = 1
         textInputView.layer.borderColor = #colorLiteral(red: 0.874435842, green: 0.8745588064, blue: 0.8743970394, alpha: 1)
         textInputView.layer.cornerRadius = 22
+    }
+}
+
+//MARK:- Keyboard event
+extension ChatViewController {
+    private func addKeyboardNotification() {
+        NotificationCenter.default.addObserver(
+          self,
+          selector: #selector(keyboardWillShow),
+          name: UIResponder.keyboardWillShowNotification,
+          object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+          self,
+          selector: #selector(keyboardWillHide),
+          name: UIResponder.keyboardWillHideNotification,
+          object: nil
+        )
+      }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+      if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+        let keybaordRectangle = keyboardFrame.cgRectValue
+        let keyboardHeight = keybaordRectangle.height
+        movingView.transform = CGAffineTransform(translationX: 0, y: -keyboardHeight)
+      }
+    }
+      
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        movingView.transform = .identity
     }
 }
