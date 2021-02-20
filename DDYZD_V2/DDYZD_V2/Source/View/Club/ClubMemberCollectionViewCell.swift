efd65807a38ad5d35be478183710d0d12bffa323
kimxwan0319 <kimxwan0319@naver.com>
2021-02-21 00:23:31 +0900

    CREATE; swift file about club member collection view

---
+++ DDYZD_V2/DDYZD_V2/Source/View/Club/ClubMemberCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ClubMemberCollectionViewCell.swift
+//  DDYZD_V2
+//
+//  Created by 김수완 on 2021/02/21.
+//
+
+import UIKit
+
+class ClubMemberCollectionViewCell: UICollectionViewCell {
+    
+}
