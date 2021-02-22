8e545f613dede835be51c1be0bd633b8f91c8382
kimxwan0319 <kimxwan0319@naver.com>
2021-02-22 15:46:27 +0900

    UPDATE' OtherUserPageViewModel

--- DDYZD_V2/DDYZD_V2/Source/ViewModel/Personal/OtherUserPageViewModel.swift
+++ DDYZD_V2/DDYZD_V2/Source/ViewModel/Personal/OtherUserPageViewModel.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+import RxCocoa
+import RxSwift
+
+
+class OtherUserPageViewModel: ViewModelProtocol {
+    
+    private var gcn = ""
+    private let disposeBag = DisposeBag()
+    
+    init(gcn: GCN) {
+        self.gcn = gcn.gcn
+    }
+    
+    struct input {
+        let getMyInfo: Driver<Void>
+    }
+    
+    struct output {
+        let userInfo: PublishRelay<UserInfo>
+        let belongClub: PublishRelay<[Club]>
+    }
+    
+    func transform(_ input: input) -> output {
+        let personalAPI = PersonalAPI()
+        let userInfo = PublishRelay<UserInfo>()
+        let belongClub = PublishRelay<[Club]>()
+        
+        input.getMyInfo.asObservable().subscribe(onNext: {
+            personalAPI.getUserInfo(gcn: self.gcn).asObservable().subscribe(onNext: { data, res in
+                switch res {
+                case .success:
+                    userInfo.accept(data!)
+                    if data!.clubs.count == 1 {
+                        belongClub.accept(data!.clubs+[Club(club_id: -1, club_name: "", club_image: "")])
+                    } else {
+                        belongClub.accept(data!.clubs)
+                    }
+                default:
+                    print("get user info: \(res)")
+                }
+            })
+            .disposed(by: self.disposeBag)
+        })
+        .disposed(by: disposeBag)
+        
+        return output(userInfo: userInfo, belongClub: belongClub)
+    }
+}
