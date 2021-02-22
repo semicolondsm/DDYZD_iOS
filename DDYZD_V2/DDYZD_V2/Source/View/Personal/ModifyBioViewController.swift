70ded2fd96d8333bd75ed59148af0b1e26d78989
kimxwan0319 <kimxwan0319@naver.com>
2021-02-22 17:25:32 +0900

    CREATE; VC file about modify bio

---
+++ DDYZD_V2/DDYZD_V2/Source/View/Personal/ModifyBioViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ModifyBioViewController.swift
+//  DDYZD_V2
+//
+//  Created by 김수완 on 2021/02/22.
+//
+
+import UIKit
+
+class ModifyBioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
